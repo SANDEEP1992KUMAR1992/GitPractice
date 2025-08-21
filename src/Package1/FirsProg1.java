14d2ea9f34f0a9155aef55b15b64f1a80041373f
sandeep <rautsandeepkumar@gmail.com>
2025-08-21 09:26:54 +0530

    added sopln

--- src/Package1/FirsProg1.java
+++ src/Package1/FirsProg1.java
@@ -4,6 +4,9 @@ public class FirsProg1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("This is my first program in Java");
+		System.out.println("This is my first program in Java-2");
 
 	}
 
